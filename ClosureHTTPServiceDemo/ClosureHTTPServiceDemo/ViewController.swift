7bf4b42aced6e7a4901d6588063e0c63b294400b
Tawan-Github <raphat02.mainirl@gmail.com>
2023-12-31 14:30:53 +0700

    Imported project files

---
+++ ClosureHTTPServiceDemo/ClosureHTTPServiceDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ClosureHTTPServiceDemo
+//
+//  Created by Raphat Aektantidamrong on 31/12/2566 BE.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
